3d2445d5a2bb04ea422c49d6f248c8e0e211369e
Stephen Stewart <stephen.stewart@carisenda.com>
2015-11-06 11:50:14 +0000

    add this

---
+++ teams.js
@@ -0,0 +1,15 @@
+
+LaunchpadTeams = function(options) {
+  this.requestParams = {'openid.ns.lp': 'http://ns.launchpad.net/2007/openid-teams'};
+  if (options.teams && options.teams.length) {
+    this.requestParams['openid.lp.query_membership'] = options.teams.join(',');
+  }
+}
+
+LaunchpadTeams.prototype.fillResult = function(params, result) {
+  if (params['openid.lp.is_member'] && params['openid.lp.is_member'].length) {
+    result['teams'] = params['openid.lp.is_member'].split(',');
+  }
+};
+
+module.exports = LaunchpadTeams;
