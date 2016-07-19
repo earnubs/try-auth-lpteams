12e7b807a7c8026173aaa5e5057be2ebaca26ebd
Stephen Stewart <stephen.stewart@carisenda.com>
2016-07-19 21:28:29 +0100

    css modules ftw, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ module.exports = {
     './client/index',
   ],
   output: {
-    publicPath: '/dist/',
-    filename: 'bundle.js'
     path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
@@ -20,6 +20,12 @@ module.exports = {
       {
         test: /\.js$/,
         loaders: ['babel'],
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'client')
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
         exclude: /node_modules/,
         include: path.join(__dirname, 'client')
       }
