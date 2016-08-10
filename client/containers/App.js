83022eea97f894e9527e24d3a468b05b9fc55143
Stephen Stewart <stephen.stewart@carisenda.com>
2016-08-10 17:37:01 +0100

    add home route & move nav to root container (App)

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,14 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
   render() {
     return (
-    <div>
-      {this.props.children}
-    </div>
-    )
+      <div>
+        <div className={'b-nav'}>
+          <div className={'b-nav__wrap'}>
+            <div className={'b-nav__homelink'}>
+              <Link to='/'>Snapfui</Link>
+            </div>
+          </div>
+        </div>
+        {this.props.children}
+      </div>
+    );
   }
 }
 
 App.propTypes = {
-}
+  children: PropTypes.object
+};
